1d8cdbb50ad7536081db22a50691ad89d8a8913b
alan <690974910@qq.com>
2021-03-19 15:23:17 +0800

    设计模式-命令模式

--- src/main/java/designPatterns/command/Client.java
+++ src/main/java/designPatterns/command/Client.java
@@ -1,7 +1,7 @@
 package designPatterns.command;
 
 /**
- * 命令模式（命令对象可以把行动及其参数封装起来，于是这些行动可以被重复多次执行 取消后又再重做 这些都是现代大型应用程序所必须的功能，即“撤销”及“重复”。）
+ * 命令模式（将一个请求封装成一个对象，从而让你使用不同的请求把客户端参数化，对请求排队或者记录请求日志，可以提供命令的撤销和恢复功能。）
  */
 public class Client {
     public static void main(String[] args) {
