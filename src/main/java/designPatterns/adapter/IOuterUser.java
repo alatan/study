7632883b07065c954a7a7bb4c681d9fc0e691ab0
alan <690974910@qq.com>
2021-03-22 15:37:36 +0800

    设计模式-适配器模式

--- src/main/java/designPatterns/adapter/IOuterUser.java
+++ src/main/java/designPatterns/adapter/IOuterUser.java
@@ -2,6 +2,9 @@ package designPatterns.adapter;
 
 import java.util.Map;
 
+/**
+ *  Adaptee源角色（你想把谁转换成目标角色，这个“谁”就是源角色，它是已经存在的、运行良好的类或对象，经过适配器角色的包装，它会成为一个崭新、靓丽的角色。）
+ */
 public interface IOuterUser {
     //基本信息，比如名称、性别、手机号码等
     public Map getUserBaseInfo();
--- src/main/java/designPatterns/adapter/OuterUser.java
+++ src/main/java/designPatterns/adapter/OuterUser.java
@@ -3,9 +3,7 @@ package designPatterns.adapter;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- *  Adaptee源角色（你想把谁转换成目标角色，这个“谁”就是源角色，它是已经存在的、运行良好的类或对象，经过适配器角色的包装，它会成为一个崭新、靓丽的角色。）
- */
+
 public class OuterUser implements IOuterUser {
     /**
      * 用户的基本信息
