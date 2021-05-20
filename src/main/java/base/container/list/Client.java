53ea330b759ff098a9b581d54248d4888fc02f25
alan <690974910@qq.com>
2021-05-20 18:10:11 +0800

    Java基础-容器list

---
+++ src/main/java/base/container/list/Client.java
@@ -0,0 +1,23 @@
+package base.container.list;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.LinkedList;
+
+public class Client {
+    public static void main(String[] args) {
+        ArrayList arrayList = new ArrayList();
+        arrayList.add("a");
+        arrayList.sort(new Comparator() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                return 0;
+            }
+        });
+        System.out.println(arrayList.get(0));
+        LinkedList linkedList = new LinkedList();
+        linkedList.add("a");
+        linkedList.addFirst("b");
+        System.out.println(linkedList.getFirst());
+    }
+}
