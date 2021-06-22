37d9ebd7037857477338e5700d3408bb5fbf6aa8
alan <690974910@qq.com>
2021-06-22 16:20:19 +0800

    数据结构

---
+++ src/main/java/algorithm/list/ArrayOpt.java
@@ -0,0 +1,47 @@
+package algorithm.list;
+
+/**
+ * 数组
+ * 参考 https://www.pdai.tech/md/algorithm/alg-basic-array.html
+ */
+public class ArrayOpt {
+
+    /**
+     * 把数组中的 0 移到末尾
+     */
+    public static void moveZeroes(int[] nums) {
+        int idx = 0;
+        for (int num : nums) {
+            if (num != 0) {
+                nums[idx++] = num;
+            }
+        }
+        while (idx < nums.length) {
+            nums[idx++] = 0;
+        }
+    }
+
+    /**
+     * 找出数组中最长的连续 1
+     * @param nums
+     * @return
+     */
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0, cur = 0;
+        for (int x : nums) {
+            cur = x == 0 ? 0 : cur + 1;
+            max = Math.max(max, cur);
+        }
+        return max;
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 0, 3, 12};
+        moveZeroes(nums);
+//        for (int temp: nums){
+//            System.out.println(temp);
+//        }
+        System.out.println(findMaxConsecutiveOnes(nums));
+    }
+}
---
+++ src/main/java/algorithm/list/HashTable.java
@@ -0,0 +1,38 @@
+package algorithm.list;
+
+import java.util.HashMap;
+
+/**
+ * 哈希表
+ *
+ * 链接：https://www.pdai.tech/md/algorithm/alg-basic-hashtable.html
+ */
+public class HashTable {
+
+    /**
+     * 数组中两个数的和为给定值
+     *
+     * 可以先对数组进行排序，然后使用双指针方法或者二分查找方法。
+     * 这样做的时间复杂度为 O(NlogN)，空间复杂度为 O(1)。
+     * 用 HashMap 存储数组元素和索引的映射，在访问到 nums[i] 时，判断 HashMap 中是否存在 target - nums[i]，
+     * 如果存在说明 target - nums[i] 所在的索引和 i 就是要找的两个数。
+     * 该方法的时间复杂度为 O(N)，空间复杂度为 O(N)，使用空间来换取时间。
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> indexForNum = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (indexForNum.containsKey(target - nums[i])) {
+                return new int[]{indexForNum.get(target - nums[i]), i};
+            } else {
+                indexForNum.put(nums[i], i);
+            }
+        }
+        return null;
+    }
+
+
+
+}
